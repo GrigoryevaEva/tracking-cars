e477a1836c72308e0375ea0a155eff7a20dc9259
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-08 22:47:18 +0300

    add providers&routers in App

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,11 @@
+import { Providers } from "./providers"
+import { AppRouter } from "./routers"
 
 function App() {
   return (
-    <div></div>
+    <Providers>
+      <AppRouter />
+    </Providers>
   );
 }
 
