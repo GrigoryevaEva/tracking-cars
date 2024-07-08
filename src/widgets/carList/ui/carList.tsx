91e7509407e7862db4c33d1432257d0c4158a60f
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-08 22:48:53 +0300

    create widget carList

---
+++ src/widgets/carList/ui/carList.tsx
@@ -0,0 +1,10 @@
+
+import style from './style.module.scss'
+
+export const CarList = () => {
+  return (
+    <div className={style.root}>
+      <div className={style.card}>cardCar</div>
+    </div>
+  )
+}
