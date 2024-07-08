638a0050603c7fcd636ddc05b57a19a7d79f446d
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-08 22:48:26 +0300

    create home page

---
+++ src/pages/home/ui/home.tsx
@@ -0,0 +1,13 @@
+import { CarList } from "../../../widgets/carList"
+import { Header } from "../../../widgets/header"
+import { Map } from "../../../widgets/map"
+
+export const Home = () => {
+  return (
+    <>
+      <Header />
+      <CarList />
+      <Map />
+    </>
+  )
+}
