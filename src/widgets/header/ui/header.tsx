eb258a2ea04e84425afe9b7ab63a33ac55e7050f
Eva <grigoryeva.eva.2704@mail.ru>
2024-07-09 21:14:15 +0300

    add header

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,7 +1,8 @@
+import style from './style.module.scss'
 
 export const Header = () => {
   return (
-    <header>
+    <header className={style.header}>
       <h1>Tracking cars</h1>
     </header>
   )
